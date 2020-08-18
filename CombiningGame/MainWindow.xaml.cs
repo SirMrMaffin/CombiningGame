e6253f8ae4ff64fb72e50f996677f84fbaab574b
Denys Honcharov <denios844@gmail.com>
2020-08-18 19:16:17 +0200

    #2 - Working basic functionality

--- CombiningGame/MainWindow.xaml.cs
+++ CombiningGame/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CombiningGame
 {
@@ -20,9 +10,70 @@ namespace CombiningGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int RightAnswersScore;
+        private int WrongAnswersScore;
+        private int RightAnswer;
+        private readonly Stopwatch StopWatch = new Stopwatch();
+        private bool running;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnStartGameButtonClicked(object sender, RoutedEventArgs e)
+        {
+            RightAnswersScore = 0;
+            WrongAnswersScore = 0;
+            StopWatch.Start();
+            running = true;
+        }
+
+        private void CheckStopWatch()
+        {
+            var stopWatchElapsedMilliseconds = StopWatch.ElapsedMilliseconds;
+            var timeInput = int.Parse(TimeInputTextBox.Text) * 60 * 1000;
+
+            if (stopWatchElapsedMilliseconds > timeInput)
+            {
+                running = false;
+                StopWatch.Stop();
+                MessageBox.Show($"Right: {RightAnswersScore}\nWrong: {WrongAnswersScore}");
+            }
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                if (running == true)
+                {
+                    CheckAnswer(RightAnswer, AnswerInputTextBox.Text);
+                    GenerateRandomNumbers();
+                    CheckStopWatch();
+                }
+            }
+        }
+
+        private void GenerateRandomNumbers()
+        {
+            var random = new Random();
+            var firstNumber = random.Next(1, 10);
+            var secondNumber = random.Next(1, 10);
+            NumbersDisplayLabel.Content = $"{firstNumber} * {secondNumber} =";
+            RightAnswer = firstNumber * secondNumber;
+        }
+
+        private void CheckAnswer(int rightAnswer, string userAnswer)
+        {
+            if (rightAnswer.Equals(int.Parse(userAnswer)))
+            {
+                RightAnswersScore++;
+            }
+            else
+            {
+                WrongAnswersScore++;
+            }
         }
     }
 }
